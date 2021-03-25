83298229d7f2e1d21e16d8c41209b635399fba4b
Alfonso Gabriel Gabriel <441262@mcpsmd.net>
2021-03-25 16:50:06 +0000

    Fixes the problem where players can't place symbols on taken spots.

--- ttt.py
+++ ttt.py
@@ -14,7 +14,13 @@ def print_board():
 
 # Inserts a move at a given row & column
 def make_move(row, col, symbol):
-  board[row][col] = symbol
+  if not space_taken(row, col):
+    board[row][col] = symbol
+  else:
+    print('Space Already Taken - Try Again')
+    r_choice = int(input('Which row would you like to choose? '))
+    c_choice = int(input('Which row would you like to choose? '))
+    make_move(r_choice, c_choice, symbol)
 
 # Returns true when the game is over 
 # Note: Just a stub. Doesn't work yet
@@ -26,7 +32,44 @@ def change_turn():
   global turn
   turn = (turn + 1) % 2
 
+# Prints a title screen for the game
+def welcome_screen():
+  print('''
+d888888b d888888b  .o88b. 
+`~~88~~'   `88'   d8P  Y8 
+   88       88    8P      
+   88       88    8b      
+   88      .88.   Y8b  d8 
+   YP    Y888888P  `Y88P' 
+                          
+                          
+d888888b  .d8b.   .o88b.  
+`~~88~~' d8' `8b d8P  Y8  
+   88    88ooo88 8P       
+   88    88~~~88 8b       
+   88    88   88 Y8b  d8  
+   YP    YP   YP  `Y88P'  
+                          
+                          
+d888888b  .d88b.  d88888b 
+`~~88~~' .8P  Y8. 88'     
+   88    88    88 88ooooo 
+   88    88    88 88~~~~~ 
+   88    `8b  d8' 88.     
+   YP     `Y88P'  Y88888P 
+  ''')
+
+# returns true if space at index row, col has been taken
+# returns false otherwise
+def space_taken(row, col):
+  if board[row][col] != '_':
+    return True
+  else:
+    return False
+
 while not is_game_over():
+  welcome_screen()
+
   # Print the board and whose turn it is
   print_board()
   print('Player {}'.format(turn+1))
